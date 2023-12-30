edfd853ad9ebe92a2481de1e1570a30315b44a3f
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2023-12-31 03:02:39 +0530

    Time: 1 ms (16.09%) | Memory: 43.4 MB (22.67%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -7,7 +7,7 @@ class Solution {
             int mid = (l+r)/2;
             if(target == nums[mid] || nums[l] == target || nums[r] == target) return true;
             if(nums[l] == nums[mid]) l++;
-            else if(nums[mid] == nums[r]) r--;
+            //else if(nums[mid] == nums[r]) r--;
             else if(nums[mid] < target){
                 if(nums[r] >= nums[mid] && nums[r] < target) r = mid-1;
                 else l = mid+1;
