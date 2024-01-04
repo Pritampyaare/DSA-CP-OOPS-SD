f1fd6feb86fbf0886ad009088fb6c17be30fb072
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-04 12:34:58 +0530

    Time: 66 ms (8.30%) | Memory: 59.9 MB (24.15%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
@@ -1,9 +1,13 @@
 class Solution {
     public int minOperations(int[] nums) {
         int[] freq = new int[1000001];
-        for(int e: nums) freq[e]++;
+        HashSet<Integer> set = new HashSet<>();
+        for(int e: nums){
+            freq[e]++;
+            set.add(e);
+        } 
         int count=0;
-        for(int i=1; i<freq.length; i++){
+        for(Integer i: set){
             if(freq[i]==1){
                 return -1;
             }else if(freq[i]==2){
