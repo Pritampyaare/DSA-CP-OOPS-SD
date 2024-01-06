9b96c0ec91face138780ce4e48cfbcf29d7bc82a
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-07 03:55:12 +0530

    Time: 8 ms (100.00%) | Memory: 44.4 MB (50.00%) - LeetSync

--- 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.java
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.java
@@ -1,13 +1,19 @@
 class Solution {
     public int minimumOperationsToMakeEqual(int x, int y) {
+        int[] dp = new int[11000];
+        Arrays.fill(dp, -1);
+        
+        return minimumOperationsToMakeEqualH(x, y, dp);
+    }
+    public int minimumOperationsToMakeEqualH(int x, int y, int[] dp) {
         
         if(x<=y) return y-x;
-        
+        if(dp[x]!=-1) return dp[x];
         int res = x-y;
-        res = Math.min(res, 1+x%5+minimumOperationsToMakeEqual(x/5, y));
-        res = Math.min(res, 1+5-x%5+minimumOperationsToMakeEqual(x/5+1, y));
-        res = Math.min(res, 1+x%11+minimumOperationsToMakeEqual(x/11, y));
-        res = Math.min(res, 1+11-x%11+minimumOperationsToMakeEqual(x/11+1, y));
-        return res;
+        res = Math.min(res, 1+x%5+minimumOperationsToMakeEqualH(x/5, y, dp));
+        res = Math.min(res, 1+5-x%5+minimumOperationsToMakeEqualH(x/5+1, y, dp));
+        res = Math.min(res, 1+x%11+minimumOperationsToMakeEqualH(x/11, y, dp));
+        res = Math.min(res, 1+11-x%11+minimumOperationsToMakeEqualH(x/11+1, y, dp));
+        return dp[x]=res;
     }
 }
