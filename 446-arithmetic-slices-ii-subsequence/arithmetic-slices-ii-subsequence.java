706e270c5d06f6f6789e3c4458f5cf14b1673d47
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-08 00:40:31 +0530

    Time: 106 ms (91.28%) | Memory: 70.8 MB (85.46%) - LeetSync

--- 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
@@ -1,23 +1,45 @@
 class Solution {
-    public int numberOfArithmeticSlices(int[] nums) {
-        int n=nums.length, ans=0;
-        HashMap<Integer, Integer>[] dp = new HashMap[n];
-        for(int i=0; i<n; i++) dp[i] = new HashMap<>();
-        //Arrays.fill(dp, new HashMap<>());
-
-        for(int i=1; i<n; i++){
-            for(int j=i-1; j>=0; j--){
-                long cdl = (long)nums[i]-(long)nums[j];
-                if(cdl >= Integer.MAX_VALUE || cdl <= Integer.MIN_VALUE) continue;
-                int cd = (int)cdl;
-                if(dp[j].containsKey(cd)){
-                    dp[i].put(cd, dp[j].get(cd)+dp[i].getOrDefault(cd,0)+1);
-                    ans += dp[j].get(cd);
-                }else{
-                    dp[i].put(cd, dp[i].getOrDefault(cd, 0)+1);
-                }
-            }
+    int count=0;
+    public int numberOfArithmeticSlices(int[] A) {
+    int res = 0;
+    Map<Integer, Integer>[] map = new Map[A.length];
+		
+    for (int i = 0; i < A.length; i++) {
+        map[i] = new HashMap<>(i);
+        	
+        for (int j = 0; j < i; j++) {
+            long diff = (long)A[i] - A[j];
+            if (diff <= Integer.MIN_VALUE || diff > Integer.MAX_VALUE) continue;
+        		
+            int d = (int)diff;
+            int c1 = map[i].getOrDefault(d, 0);
+            int c2 = map[j].getOrDefault(d, 0);
+            res += c2;
+            map[i].put(d, c1 + c2 + 1);
         }
-        return ans;
     }
+		
+    return res;
 }
+    
+}
+// class Solution {
+//     int count=0;
+//     public int numberOfArithmeticSlices(int[] nums) {
+//         helper(nums, 0, new ArrayList<>());
+//         return count;
+//     }
+//     public void helper(int[] nums, int idx, ArrayList<Integer> al){
+//         int size = al.size();
+//         if(idx==nums.length){
+//             if(al.size()>=3) count++;
+//             return;
+//         }
+//         if(size<2 || (long)nums[idx]-(long)al.get(size-1)==(long)al.get(al.size()-1)-(long)al.get(al.size()-2)){
+//             ArrayList<Integer> temp = new ArrayList<>();
+//             temp.addAll(al); temp.add(nums[idx]);
+//             helper(nums, idx+1, temp);
+//         }
+//         helper(nums, idx+1, al);
+//     }
+// }
