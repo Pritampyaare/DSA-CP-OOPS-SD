5db2dd3739ecd55cd485056e285b9e6705c06522
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-12 14:26:26 +0530

    Time: 147 ms (59.35%) | Memory: 41.2 MB (49.88%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,27 +1,22 @@
 class Solution {
-    static char[][] bard;
-    static String word;
-    public boolean exist(char[][] bard, String word) {
-        this.bard = bard;
-        this.word = word;
-        for(int i=0; i<bard.length; i++){
-            for(int j=0; j<bard[0].length; j++){
-                boolean[][] visited = new boolean[bard.length][bard[0].length];
-                if(helper(bard, word, i, j, 0, visited) == true) return true;
+    public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(find(board, word, visited, i, j, 0)) return true;
             }
         }
         return false;
     }
-    private boolean helper(char[][] bard, String word, int row, int col, int idx, boolean[][] visited){
-        if(row < 0 || row > bard.length-1 || col < 0 || col > bard[0].length-1 || visited[row][col] == true || bard[row][col] != word.charAt(idx)) return false;
+    public boolean find(char[][] board, String word, boolean[][] visited, int row, int col, int idx){
+        if(row<0 || row>=board.length || col<0 || col>=board[0].length || board[row][col] != word.charAt(idx) || visited[row][col]) return false;
         visited[row][col] = true;
-        if(idx == word.length()-1) return true;
-        boolean ans = false;
-        ans = ans || helper(bard, word, row+1, col, idx+1, visited);
-        ans = ans || helper(bard, word, row-1, col, idx+1, visited);
-        ans = ans || helper(bard, word, row, col+1, idx+1, visited);
-        ans = ans || helper(bard, word, row, col-1, idx+1, visited);
-        if(ans == false) visited[row][col] = false;
+        if(idx==word.length()-1) return true;
+        boolean ans = find(board, word, visited, row+1, col, idx+1);
+        ans = ans || find(board, word, visited, row-1, col, idx+1);
+        ans = ans || find(board, word, visited, row, col+1, idx+1);
+        ans = ans || find(board, word, visited, row, col-1, idx+1);
+        visited[row][col] = false;
         return ans;
     }
 }
