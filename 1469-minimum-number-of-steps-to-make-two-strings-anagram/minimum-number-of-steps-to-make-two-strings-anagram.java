515eab21ad280e71b4c277db2333bfdcfd716ab7
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-13 12:51:45 +0530

    Time: 8 ms (76.51%) | Memory: 45.3 MB (18.05%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,17 +1,12 @@
 class Solution {
     public int minSteps(String s, String t) {
-        int[] freqs = new int[26], freqt = new int[26], visited = new int[26];
-        for(int i=0; i<s.length(); i++){
-            freqs[s.charAt(i)-'a']++;
-            freqt[t.charAt(i)-'a']++;
+        if(s.equals(t)) return 0;
+        int n=s.length(), ans=0;
+        int[] freq = new int[26];
+        for(int i=0; i<n; i++){
+            freq[s.charAt(i)-'a']++; freq[t.charAt(i)-'a']--;
         }
-        int changes=0;
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(visited[ch-'a']==1) continue;
-            changes += (freqs[ch-'a']-freqt[ch-'a']<0 ? 0: freqs[ch-'a']-freqt[ch-'a']);
-            visited[ch-'a']=1;
-        }
-        return changes;
+        for(int i: freq) if(i>0) ans+=i;
+        return ans;
     }
 }
