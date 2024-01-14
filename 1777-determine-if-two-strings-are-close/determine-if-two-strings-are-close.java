1b75703057633469fedebf3b5d3e89bbc6bcb2c1
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-14 23:10:17 +0530

    Time: 11 ms (78.03%) | Memory: 45.7 MB (20.97%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,31 +1,17 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
-        for (char ch : word1.toCharArray()) {
-            freq1[ch - 'a']++;
+        if(word1.length()!=word2.length()) return false;
+        int[] freq1 = new int[26], freq2 = new int[26];
+        for(int i=0; i<word1.length(); i++){
+            freq1[word1.charAt(i)-'a']++;
+            freq2[word2.charAt(i)-'a']++;
         }
-
-        for (char ch : word2.toCharArray()) {
-            freq2[ch - 'a']++;
+        for(int i=0; i<26; i++){
+            if(freq1[i]==0 && freq2[i]!=0) return false;
+            if(freq2[i]==0 && freq1[i]!=0) return false;
         }
-
-        for (int i = 0; i < 26; i++) {
-            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
-                return false;
-            }
-        }
-
-        Arrays.sort(freq1);
-        Arrays.sort(freq2);
-
-        for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) {
-                return false;
-            }
-        }
-
+        Arrays.sort(freq1); Arrays.sort(freq2);
+        for(int i=25; i>=0; i--) if(freq1[i] != freq2[i]) return false;
         return true;
     }
 }
