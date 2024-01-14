ff83b9d60b12672facd757751ab1caa99fa318b6
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-14 22:13:48 +0530

    Update minimum-number-of-steps-to-make-two-strings-anagram.java

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -4,7 +4,8 @@ class Solution {
         int n=s.length(), ans=0;
         int[] freq = new int[26];
         for(int i=0; i<n; i++){
-            freq[s.charAt(i)-'a']++; freq[t.charAt(i)-'a']--;
+            freq[s.charAt(i)-'a']++; 
+            freq[t.charAt(i)-'a']--;
         }
         for(int i: freq) if(i>0) ans+=i;
         return ans;
