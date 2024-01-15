94c31d1d314e495831e1bf74c00acb26b683e910
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-15 13:24:05 +0530

    Time: 25 ms (99.08%) | Memory: 93.3 MB (58.23%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         int[] arr = new int[100001];
-        
+        //for winner mark 1, for losser mark -1 or decrement if already losser
         for(int i=0; i<matches.length; i++){
             int winner = matches[i][0], losser = matches[i][1];
-            if(arr[winner]>-1) arr[winner]=1;
-            arr[losser] = arr[losser]>0 ? -1 : arr[losser]-1;
+            if(arr[winner]>-1) arr[winner]=1; //mark winner only if not marked as losser
+            arr[losser] = arr[losser]>0 ? -1 : arr[losser]-1; //mark -1 or decrement
         }
         List<Integer> winnersList = new ArrayList<>(), lossersList = new ArrayList<>();
         for(int i=0; i<arr.length; i++){
