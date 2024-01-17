06ddb79ab79970a9a7cf4d13f0e9a53dcafe20d1
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-17 11:01:12 +0530

    Time: 2 ms (98.10%) | Memory: 41.9 MB (16.50%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         int[] freq = new int[2002];
-        int[] occur = new int[1001];
+        HashSet<Integer> set = new HashSet<>();
 
         for(int e : arr) freq[e+1000]++;
         for(int e : freq){
             if(e==0) continue;
-            if(occur[e] != 0) return false;
-            occur[e] = 1;
+            if(set.contains(e)) return false;
+            set.add(e);
         }
         return true;
     }
