bfe6f25a0237c027e2d8060a13b58ce976ce4781
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-21 12:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (7.49%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] yes = new int[n];
+        int[] no = new int[n];
+
+        yes[0] = Math.max(nums[0], 0);
+        for(int i=1; i<n; i++){
+            yes[i] = Math.max(yes[i-1], no[i-1]+nums[i]);
+            no[i] = yes[i-1];
+        }
+        return yes[n-1];
+    }
+}
