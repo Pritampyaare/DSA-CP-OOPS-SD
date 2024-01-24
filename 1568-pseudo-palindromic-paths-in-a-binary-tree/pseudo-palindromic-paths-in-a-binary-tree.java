ffcdaa85a8fabb74c107f5c6bcd521b0d5ca2153
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-01-25 00:16:40 +0530

    Time: 11 ms (77.94%) | Memory: 71.5 MB (30.96%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -1,32 +1,49 @@
-
-import java.util.Stack;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int pseudoPalindromicPaths(TreeNode root) {
-        int count = 0, path = 0;
+    public int pseudoPalindromicPaths (TreeNode root) {
+        int[] freq = new int[10];
+        return helper(root, freq);
+    }
+    public int helper(TreeNode root, int[] freq){
+        freq[root.val]++;
+        if(root.left==null && root.right==null){
+            int ans = isValid(freq) ? 1 : 0;
+            freq[root.val]--;
+            return ans;
+        }
+        int sum=0;
+        if(root.left!=null){
+            sum += helper(root.left, freq);
+        }
+        if(root.right!=null){
+            sum += helper(root.right, freq);
+        }
+        freq[root.val]--;
+        return sum;
+    }
 
-        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
-        stack.push(new Pair<>(root, 0));
-
-        while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> p = stack.pop();
-            TreeNode node = p.getKey();
-            path = p.getValue();
-
-            if (node != null) {
-                path = path ^ (1 << node.val);
-
-                if (node.left == null && node.right == null) {
-                    if ((path & (path - 1)) == 0) {
-                        ++count;
-                    }
-                } else {
-                    stack.push(new Pair<>(node.right, path));
-                    stack.push(new Pair<>(node.left, path));
-                }
+    public boolean isValid(int[] freq){
+        boolean flag = false;
+        for(int i=1; i<=9; i++){
+            if(freq[i]%2==1){
+                if(flag) return false;
+                flag=true;
             }
         }
-
-        return count;
+        return true;
     }
 }
