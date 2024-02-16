c3b1245e099ef57e3df7a17eaade9f021d207812
Pritam Kumar Mehta <62764494+Pritampyaare@users.noreply.github.com>
2024-02-16 10:47:44 +0530

    Time: 74 ms (22.60%) | Memory: 57.6 MB (60.59%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -3,7 +3,7 @@ class Solution {
         HashMap<Integer, Integer> hmap = new HashMap<>();
         for(int e: arr) hmap.put(e, hmap.getOrDefault(e, 0)+1);
         List<Integer> nums = new ArrayList<>(hmap.keySet());
-        Collections.sort(nums, (a,b) -> hmap.get(b)-hmap.get(a));
+        nums.sort((a,b) -> hmap.get(b)-hmap.get(a));
         while(nums.size()>0){
             int ele = nums.get(nums.size()-1);
             if(hmap.get(ele)<=k){
